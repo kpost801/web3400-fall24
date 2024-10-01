08e8182e43732b186f5baa3a1e9e0404a6184312
kpost801 <kalebpost8@gmail.com>
2024-10-01 02:22:04 +0000

    updated assignment 04

---
+++ assignments/04/templates/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.4/css/bulma.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/@vizuaalog/bulmajs@0/dist/bulma.min.js"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"></script>
+    <title>My Webpage</title>
+</head>
+
+<body class="has-navbar-fixed-top">
