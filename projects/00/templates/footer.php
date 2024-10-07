93299effe0e2d25fd739046bbcde9e255e29b190
kpost801 <kalebpost8@gmail.com>
2024-10-06 21:09:25 -0600

    created project 00 folder

---
+++ projects/00/templates/footer.php
@@ -0,0 +1,10 @@
+ <!-- BEGIN PAGE FOOTER -->
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>My Webpage</strong> by <a href="#">Your Name</a>. The source
+          code is licensed under MIT.
+        </p>
+      </div>
+    </footer>
+    <!-- END PAGE FOOTER -->
---
+++ projects/00/templates/nav.php
@@ -0,0 +1,60 @@
+    <!-- BEGIN PAGE HEADER -->
+    <header class="container">
+      <!-- BEGIN MAIN NAV -->
+      <nav
+        class="navbar is-fixed-top is-spaced has-shadow is-light"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div class="navbar-brand">
+          <a class="navbar-item" href="#">
+            <span class="icon-text">
+              <span class="icon">
+                <i class="fas fa-yin-yang"></i>
+              </span>
+              <span>&nbsp;Balance</span>
+            </span>
+          </a>
+          <a
+            role="button"
+            class="navbar-burger"
+            aria-label="menu"
+            aria-expanded="false"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item" href="#">Home</a>
+            <a class="navbar-item" href="#">About</a>
+          </div>
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <a class="button is-light">
+                  <strong>Contact us</strong>
+                </a>
+                <a class="button is-link"> Log in </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <!-- END MAIN NAV -->
+      <section class="block">
+        &nbsp;
+        <!-- this adds a little extra space between the nav and the hero -->
+      </section>
+      <!-- BEGIN HERO -->
+      <section class="hero is-info">
+        <div class="hero-body">
+          <p class="title">Hero title</p>
+          <p class="subtitle">Hero subtitle</p>
+        </div>
+      </section>
+      <!-- END HERO -->
+    </header>
+    <!-- END PAGE HEADER -->
